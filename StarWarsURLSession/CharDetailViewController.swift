a377ba7d979b383fd794aa49222db64e276356a5
Caroline Cruz <cruz.caroline08@gmail.com>
2017-12-01 19:48:41 -0500

    add detailVC

---
+++ StarWarsURLSession/CharDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CharDetailViewController.swift
+//  StarWarsURLSession
+//
+//  Created by Keshawn Swanston on 12/1/17.
+//  Copyright © 2017 Caroline Cruz. All rights reserved.
+//
+
+import UIKit
+
+class CharDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
